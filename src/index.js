b2b74bd1dd0885c2d3927f5d15628138482a79e5
Alever <aleksg2004@gmail.com>
2023-07-15 21:28:41 +0300

    0.1.0 connection established

--- src/index.js
+++ src/index.js
@@ -1,24 +1,20 @@
 const express = require('express')
-const bodyParser = require('body-parser')
-const session = require('express-session')
 const cookieSession = require('cookie-session')
 const cookieParser = require('cookie-parser')
-
+const pipedrive = require('pipedrive')
 
 const config = require('./config')
+
 
 const app = express()
 
 app.use(cookieParser());
-
 
 app.use(cookieSession({
     name: 'session',
     keys: ['key1']
 }))
 
-
-const pipedrive = require('pipedrive')
 
 const apiClient = new pipedrive.ApiClient()
 
@@ -30,32 +26,25 @@ oauth2.redirectUri = config.callbackURL
 const port = config.port
 const host = config.host
 
-console.log(config)
-
 app.listen(port, () => {
     console.log(`Server listens https://${host}:${port}`)
-}) 
+})
 
 app.get('/', async (req, res) => {
     if (req.session.accessToken !== null && req.session.accessToken !== undefined) {
-        // token is already set in the session
-        // now make API calls as required
-        // client will automatically refresh the token when it expires and call the token update callback
         const api = new pipedrive.DealsApi(apiClient)
         const deals = await api.getDeals()
 
         res.send(deals)
     } else {
         const authUrl = apiClient.buildAuthorizationUrl()
-        
+
         res.redirect(authUrl)
     }
 })
 
 app.get('/callback', (req, res) => {
     const authCode = req.query.code
-    console.log(req.query)
-    console.log(authCode)
     const promise = apiClient.authorize(authCode)
 
     promise.then(() => {
