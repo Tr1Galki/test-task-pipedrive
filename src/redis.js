e8e2ea589de53c4a5a21087c718dbeb2eef80fae
Alever <aleksg2004@gmail.com>
2023-07-15 23:47:49 +0300

    0.1.2 (version is coorect now) redis work pretty good

--- src/redis.js
+++ src/redis.js
@@ -6,29 +6,4 @@ const redis = new ioRedis(config.redisURL)
 
 console.log("Connected to Redis")
 
-
-//
-redis.set("animal", "mouse")
-
-redis.get("animal").then((result) => {
-    console.log(`Result for key animal: ${result}`)
-})
-
-redis.del("animal")
-
-redis.get("animal").then((result) => {
-    console.log(`Result for key animal: ${result}`)
-})
-
-redis.set("animal", {
-    name: "mouse",
-    size: 14
-})
-
-redis.get("animal").then((result) => {
-    console.log(`Result for key animal: ${result}`)
-})
-//
-
-
 module.exports = redis
